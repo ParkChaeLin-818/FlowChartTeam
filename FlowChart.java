11fae14a4e54895cf8394d27146b29dc6974cec7
ParkChaeLin-818 <qkrcofls6863@gmail.com>
2020-04-08 20:15:09 +0900

    교통신호와 속도 변수 생성했습니다. 기본메소드 생성 했습니다.

--- FlowChart.java
+++ FlowChart.java
@@ -1,4 +1,5 @@
 
+
 /**
  * 여기에 FlowChart 클래스 설명을 작성하십시오.
  * 
@@ -7,27 +8,15 @@
  */
 public class FlowChart
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * FlowChart 클래스의 객체 생성자
-     */
-    public FlowChart()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public static void main(String[] args){
+        char ts; // ts = 교통신호 입니다.
+        int s; // s = 속도 입니다.
+        
+        s = 10;
+        ts = ' ';
+        
+        System.out.println(ts);
+        System.out.println(s);
+        
     }
 }
